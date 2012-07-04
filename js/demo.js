04bc954daf332a6e501acca665999c2ee161bfbe
Daniel Duarte <danielddu@hotmail.com>
2012-07-04 00:30:04 -0300

    Added JSON sample to the Demo page

--- js/demo.js
+++ js/demo.js
@@ -13,7 +13,7 @@ $(function () {
             }
 
             var object = $.parseJSON(json);
-            var result = $.toJson(object, { mode: mode });
+            var result = $.toJson(object, {mode: mode});
 
             $('#result').text(result);
         } catch (ex) {
@@ -23,4 +23,11 @@ $(function () {
 
     $('#input').bind('change keyup', onParamsChange);
     $('#mode').bind('change', onParamsChange);
+
+    $('.json-sample').click(function (event) {
+        event.preventDefault();
+
+        var sampleJson = '{"anIntArray":[1,32,5,2,346,34,6], "aMixedArray":[1,32,"hello", "5,2",346,null,null,"3\\n\\r4",6], "someSpecialChars":{"spaces":["\\n\\r","\\t"    ,"\\\\"],"utf":"你好","你好":"??"},"nestedArrays":["x",[],[[[3],4,[[]],[[],[]],[5]]],[],66],"":{"{}":[{},"{1,2,3}"]}}';
+        $('#input').val(sampleJson).change();
+    });
 });
