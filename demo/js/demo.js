1b028bd11252e470069c9acd3d83c77cf6a2757e
Daniel Duarte <danielddu@hotmail.com>
2012-07-17 21:08:57 -0300

    Improvements in options and demo (not applyed to src/)

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -4,29 +4,73 @@ $(function () {
     'use strict';
 
     var onParamsChange = function () {
+        // Options
+        var preset = $('#preset').val();
+        var presetValues = {
+            "(default)" : undefined,
+            "pretty"    : 'pretty',
+            "spaced"    : 'spaced',
+            "compact"   : 'compact'
+        };
+        preset = presetValues[preset];
+
+        var indent = $('#indent').val();
+        var indentValues = {
+            "(default)"    : undefined,
+            "4"            : 4,
+            "2"            : 2,
+            "\\t"          : "\t",
+            "---"          : "---",
+            "··"           : "··",
+            "************" : "************",
+            "X"            : "X"
+        };
+        indent = indentValues[indent];
+
+        var keyValueSeparator = $('#key-value-separator').is(':checked') ? undefined : false;
+
+
+        // Input
         var json = $('#input').val();
-        var mode = $('#mode').val();
 
         try {
             if (json === "") {
                 throw new SyntaxError("Empty input");
             }
-
             var object = $.parseJSON(json);
-            var result = $.toJSON(object, {mode: mode});
+
+            var options = {
+                preset: preset,
+                indent: indent,
+                keyValueSeparator: keyValueSeparator
+            };
+
+            // Remove options set to default
+            var optsCount = 3;
+            if (typeof options.preset === 'undefined') { delete options.preset; optsCount--; }
+            if (typeof options.indent === 'undefined') { delete options.indent; optsCount--; }
+            if (typeof options.keyValueSeparator === 'undefined') { delete options.keyValueSeparator; optsCount--; }
+            if (optsCount === 1 && typeof options.preset !== 'undefined') { options = options.preset; }
+
+            // Show call
+            var optionsStr = $.toJSON(options, 'spaced');
+            if (optionsStr === "{}") { optionsStr = ""; }
+            $('#call-str').text("$.toJSON(myObject" + (optionsStr.length === 0 ? "" : ", " + optionsStr) + ");");
+
+            var result = $.toJSON(object, options);
 
             $('#result').text(result);
+
         } catch (ex) {
             $('#result').html('<strong class="error">' + ex.toString() + '</strong>');
         }
     };
 
+    // Event management
     $('#input').bind('change keyup', onParamsChange);
-    $('#mode').bind('change', onParamsChange);
-
+    $('#preset, #indent, #key-value-separator').bind('change', onParamsChange);
     $('.json-sample').click(function (event) {
         event.preventDefault();
-
         var sampleJson = '{"anIntArray":[1,32,5,2,346,34,6], "aMixedArray":[1,32,"hello", "5,2",346,null,null,"3\\n\\r4",6], "someSpecialChars":{"spaces":["\\n\\r","\\t"    ,"\\\\"],"utf":"你好","你好":"??"},"nestedArrays":["x",[],[[[3],4,[[]],[[],[]],[5]]],[],66],"":{"{}":[{},"{1,2,3}"]}}';
         $('#input').val(sampleJson).change();
     });
--- demo/js/lib/jquery.json.js
+++ demo/js/lib/jquery.json.js
@@ -8,65 +8,116 @@
 
 /*globals jQuery*/
 
+/*
+ * @todo Treat Infinity value.
+ */
 (function ($) {
     'use strict';
+
+    var _defaultOptions = {
+        'compact': {
+            preset: 'compact',
+            indent: "",
+            keyValueSeparator: false,
+            undefinedReplace: null,
+            functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true
+        },
+        'spaced': {
+            preset: 'spaced',
+            indent: "",
+            keyValueSeparator: true,
+            undefinedReplace: null,
+            functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true
+        },
+        'pretty': {
+            preset: 'pretty',
+            indent: 4,
+            keyValueSeparator: true,
+            undefinedReplace: null,
+            functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true
+        }
+    };
 
     var _modes = {
         'compact': {
             undefinedReplace: null,
             functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true,
             array: {
                 separator: ",",
                 indent: "",
                 open: "[",
+                afterOpen: "",
                 close: "]",
-                beforeClose: ""
+                afterLast: ""
             },
             object: {
                 separator: ",",
                 indent: "",
                 open: "{",
+                afterOpen: "",
                 close: "}",
-                beforeClose: "",
+                afterLast: "",
                 keyValueSeparator: ":"
             }
         },
         'spaced': {
             undefinedReplace: null,
             functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true,
             array: {
                 separator: ", ",
                 indent: "",
                 open: "[",
+                afterOpen: "",
                 close: "]",
-                beforeClose: ""
+                afterLast: ""
             },
             object: {
                 separator: ", ",
                 indent: "",
                 open: "{",
+                afterOpen: "",
                 close: "}",
-                beforeClose: "",
+                afterLast: "",
                 keyValueSeparator: ": "
             }
         },
         'pretty': {
             undefinedReplace: null,
             functionReplace: null,
+            compactEmptyArrays: true,
+            compactEmptyObjects: true,
+            compactOneElemArrays: true,
             array: {
                 separator: ",\n",
                 indent: "    ",
-                open: "[\n",
+                open: "[",
+                afterOpen: "\n",
                 close: "]",
-                beforeClose: "\n"
+                afterLast: "\n"
             },
             object: {
                 separator: ",\n",
                 indent: "    ",
-                open: "{\n",
+                open: "{",
+                afterOpen: "\n",
                 close: "}",
-                beforeClose: "\n",
-                keyValueSeparator: ": "
+                afterLast: "\n",
+                keyValueSeparator: ":·"
             }
         }
     };
@@ -96,43 +147,58 @@
         return value ? "true" : "false";
     };
 
+    var _arrayToJson = function (value, mode, indentLevel) {
+        var json = "";
+        var elemCount = 0, currentIndent;
+
+        var compactOne = mode.compactOneElemArrays && value.length === 1;
+        var elemsStr = [];
+        for (var i = 0; i < value.length; i++) {
+            elemsStr.push(_toJson(value[i], mode, indentLevel + (compactOne?0:1)));
+            elemCount++;
+        }
+        var isEmpty = elemCount === 0;
+
+        currentIndent = _indent(indentLevel + (compactOne?0:1), mode.array.indent);
+        json += elemsStr.join(mode.array.separator + currentIndent);
+        json += isEmpty||compactOne ? "" : mode.array.afterLast;
+        json = mode.array.open + (isEmpty||compactOne && mode.compactEmptyArrays ? "" : mode.array.afterOpen)
+            + (isEmpty||compactOne ? "" : currentIndent)
+            + json
+            + (isEmpty||compactOne && mode.compactEmptyArrays ? "" : _indent(indentLevel, mode.array.indent))
+            + mode.array.close;
+
+        return json;
+    };
+
     var _objectToJson = function (value, mode, indentLevel) {
-        var json, i, property;
+        var json, property, isEmpty;
 
         if (value === null) {
             json = "null";
         } else if (Object.prototype.toString.call(value) === '[object Array]') {
-            json = "";
-            for (i = 0; i < value.length; i++) {
-                if (json.length > 0) {
-                    json += mode.array.separator;
-                }
-                json +=  _indent(indentLevel + 1, mode.array.indent) + _toJson(value[i], mode, indentLevel + 1);
-            }
-
-            json = mode.array.open
-                + json
-                + mode.array.beforeClose
-                + _indent(indentLevel, mode.array.indent)
-                + mode.array.close;
+            json = _arrayToJson(value, mode, indentLevel);
         } else {
             json = "";
+            isEmpty = true;
             for (property in value) {
                 if (value.hasOwnProperty(property)) {
+                    isEmpty = false;
                     if (json.length > 0) {
                         json += mode.object.separator;
                     }
                     json += _indent(indentLevel + 1, mode.object.indent)
-                        + _stringToJson(property, mode, indentLevel + 1)
-                        + mode.object.keyValueSeparator
-                        + _toJson(value[property], mode, indentLevel + 1);
+                         + _stringToJson(property, mode, indentLevel + 1)
+                         + mode.object.keyValueSeparator
+                         + _toJson(value[property], mode, indentLevel + 1);
                 }
             }
 
-            json = mode.object.open
+            json += isEmpty ? "" : mode.object.afterLast;
+
+            json = mode.object.open + (isEmpty && mode.compactEmptyObjects ? "" : mode.object.afterOpen)
                 + json
-                + mode.object.beforeClose
-                + _indent(indentLevel, mode.array.indent)
+                + (isEmpty && mode.compactEmptyObjects ? "" : _indent(indentLevel, mode.array.indent))
                 + mode.object.close;
         }
 
@@ -140,7 +206,11 @@
     };
 
     var _undefinedToJson = function (value, mode, indentLevel) {
-        return null;
+        return _toJson(mode.undefinedReplace, mode, indentLevel);
+    };
+
+    var _functionToJson = function (value, mode, indentLevel) {
+        return _toJson(mode.functionReplace, mode, indentLevel);
     };
 
     var _unknownToJson = function (value, mode, indentLevel) {
@@ -167,6 +237,9 @@
         case 'undefined':
             json = _undefinedToJson(value, mode, indentLevel);
             break;
+        case 'function':
+            json = _functionToJson(value, mode, indentLevel);
+            break;
         default:
             json = _unknownToJson(value, mode, indentLevel);
         }
@@ -174,14 +247,102 @@
         return json;
     };
 
+    var _createModeFromOptions = function (options) {
+        var mode, preset, indent, keyValueSeparator, undefinedReplace, functionReplace;
+
+        // Prepare base preset mode (defaults to 'pretty')
+        if (typeof options === 'object' && typeof options.preset === 'string' && _modes.hasOwnProperty(options.preset)) {
+            preset = options.preset;
+        } else {
+            preset = 'pretty';
+        }
+
+        // Prepare default options for the selected preset
+        var op = $.extend(true, {}, _defaultOptions[preset], options);
+
+        // Prepare base mode with selected preset
+        mode = $.extend(true, {}, _modes[preset]);
+
+        // Indentation string
+        if (typeof op.indent === 'number') {
+            indent = Math.floor(op.indent);
+            indent = new Array(op.indent + 1).join(" ");
+            mode.array.indent = indent;
+            mode.object.indent = indent;
+        } else if (typeof op.indent === 'string') {
+            indent = op.indent;
+            mode.array.indent = indent;
+            mode.object.indent = indent;
+        }
+
+        // Key-Value separator
+        if (typeof op.keyValueSeparator === 'boolean') {
+            keyValueSeparator = ":" + (op.keyValueSeparator ? " " : "");
+            mode.object.keyValueSeparator = keyValueSeparator;
+        } else if (typeof op.keyValueSeparator === 'string') {
+            keyValueSeparator = ":" + op.keyValueSeparator;
+            mode.object.keyValueSeparator = keyValueSeparator;
+        }
+
+        // Undefined values replacement
+        if (typeof op.undefinedReplace === 'undefined' || typeof op.undefinedReplace === 'function' || op.undefinedReplace === null) {
+            undefinedReplace = null;
+        } else if (typeof op.undefinedReplace === 'string' || typeof op.undefinedReplace === 'number' || typeof op.undefinedReplace === 'boolean') {
+            undefinedReplace = op.undefinedReplace;
+        } else {
+            undefinedReplace = op.undefinedReplace;
+        }
+        mode.undefinedReplace = undefinedReplace;
+
+        // Undefined values replacement
+        if (typeof op.functionReplace === 'undefined' || typeof op.functionReplace === 'function' || op.functionReplace === null) {
+            functionReplace = null;
+        } else if (typeof op.functionReplace === 'string' || typeof op.functionReplace === 'number' || typeof op.functionReplace === 'boolean') {
+            functionReplace = op.functionReplace;
+        } else {
+            functionReplace = op.functionReplace;
+        }
+        mode.functionReplace = functionReplace;
+
+        // Compact empty arrays option
+        if (typeof op.compactEmptyArrays === 'boolean') {
+            mode.compactEmptyArrays = op.compactEmptyArrays;
+        }
+
+        // Compact empty arrays option
+        if (typeof op.compactEmptyObjects === 'boolean') {
+            mode.compactEmptyObjects = op.compactEmptyObjects;
+        }
+
+        // Compact one element arrays option
+        if (typeof op.compactOneElemArrays === 'boolean') {
+            mode.compactOneElemArrays = op.compactOneElemArrays;
+        }
+
+        return mode;
+    };
+
     $.toJSON = function (value, options) {
-        var _defaultOptions = {
-            mode: 'pretty'
-        };
+        var preset = null, mode;
 
-        var _options = typeof options  === 'undefined' ? _defaultOptions : options;
+        if (typeof options === 'undefined') {
+            preset = 'pretty';
+        }
+        if (typeof options === 'string') {
+            if (_modes.hasOwnProperty(options)) {
+                preset = options;
+            } else {
+                preset = 'pretty';
+            }
+        }
 
-        return _toJson(value, _modes[_options.mode], 0);
+        if (preset !== null) {
+            mode = _modes[preset];
+        } else {
+            mode = _createModeFromOptions(options);
+        }
+
+        return _toJson(value, mode, 0);
     };
 
 }(jQuery));
